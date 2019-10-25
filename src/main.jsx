65e514c7981a142db14464856a554cf5880574ea
Mike Schultz <mschultz@zingsoft.com>
2019-10-25 17:28:39 +1300

    New component structure with events and methods

--- src/main.jsx
+++ src/main.jsx
@@ -1,114 +1,101 @@
 import {zingchart} from 'zingchart';
 import React, { Component } from 'react';
+import {DEFAULT_WIDTH, DEFAULT_HEIGHT, EVENT_NAMES, METHOD_NAMES} from './constants.js';
 
-class Core extends Component{
+// One time setup globally to handle all zingchart-vue objects in the app space.
+if (!window.ZCReact) {
+    window.ZCReact = {
+        instances: {},
+        count: 0
+    };
+}
+
+class ZingChart extends Component{
+    constructor(props) {
+        super(props);
+        this.id = this.props.id || 'zingchart-react-' + window.ZCReact.count++;
+
+        // Bind all methods available to zingchart to be accessed via Refs.
+        METHOD_NAMES.forEach(name => {
+            this[name] = args => {
+                return window.zingchart.exec(this.id, name, args);
+            };
+        });
+    }
     render() {
         return (
-            <div id={this.props.id}></div>
+            <div id={this.id}></div>
         );
     }
-    //Called after the render function.
-    componentDidMount(){
-        zingchart.render({
-            id : this.props.id,
-            width: (this.props.width || 600),
-            height: (this.props.height || 400),
-            data : this.props.data,
-            theme : (this.props.theme) ? this.props.theme : "light",
-            hideprogresslogo: this.props.hideprogresslogo,
-            output: (this.props.output || 'svg'),
-        });
+    // Called after the render function.
+    componentDidMount() {
+        this.renderChart();
+
+        // Bind all events registered.
+        Object.keys(this.props).forEach(eventName => {
+            if (EVENT_NAMES.includes(eventName)) {
+              // Filter through the provided events list, then register it to zingchart.
+              window.zingchart.bind(this.id, eventName, result => {
+                this.props[eventName](result);
+              });
+            }
+          });
+
     }
     //Used to check the values being passed in to avoid unnecessary changes.
-    shouldComponentUpdate(nextProps, nextState){
-        //Lazy object comparison
-        return !(JSON.stringify(nextProps.data) === JSON.stringify(this.props.data)) ;
+    shouldComponentUpdate(nextProps) {
+        // Data change
+        if(JSON.stringify(nextProps.data) !== JSON.stringify(this.props.data)) {
+            zingchart.exec(this.id, 'setdata', {
+                data: nextProps.data,
+              });
+        // Series change
+        } else if(JSON.stringify(nextProps.series) !== JSON.stringify(this.props.series)) {
+            zingchart.exec(this.id, 'setseriesdata', {
+                data: this.props.series,
+              });
+        // Resize
+        } else if(nextProps.width !== this.props.width || nextProps.height !== this.props.height) {
+            zingchart.exec(this.id, 'resize', {
+                width: this.props.width || DEFAULT_WIDTH,
+                height: this.props.height || DEFAULT_HEIGHT,
+            });
+        }
+        // React should never re-render since ZingChart controls this component.
+        return false;
     }
-    componentWillUpdate(nextProps){
-        zingchart.exec(this.props.id, 'setdata', {
-            data : nextProps.data
-        });
+
+    renderChart() {
+        const renderObject = {
+            id: this.id,
+            width: this.props.width || DEFAULT_WIDTH,
+            height: this.props.height || DEFAULT_HEIGHT,
+            data: this.props.data,
+        };
+        if(this.props.series) {
+            renderObject.data.series = this.props.series;
+        }
+        if(this.props.theme) {
+            renderObject.defaults = this.props.theme;
+          }
+        zingchart.render(renderObject);
+    }
+    resizeChart() {
+        zingchart.exec(this.id, 'resize', {
+            width: this.props.width || DEFAULT_WIDTH,
+            height: this.props.height || DEFAULT_HEIGHT,     
+        })
+    }
+    refreshChart() {
+        zingchart.exec(this.id, 'setdata', {
+            data: this.props.data,
+        })
     }
     componentWillUnmount(){
-        zingchart.exec(this.props.id, 'destroy');
+        zingchart.exec(this.id, 'destroy');
     }
 };
 
-class Line extends Component{
-    render(){
-        var myConfig = {
-            type: "line",
-        	series : this.props.series
-        };
-        applyAttrs(this.props, myConfig);
-        return (<Core id={this.props.id} height={this.props.height} width={this.props.width} data={myConfig} theme={this.props.theme}/>);
-    }
-};
-
-class Area extends Component{
-    render(){
-        var myConfig = {
-            type: "area",
-        	series : this.props.series
-        };
-        applyAttrs(this.props, myConfig);
-        return (<Core id={this.props.id} height={this.props.height} width={this.props.width} data={myConfig} theme={this.props.theme}/>);
-    }
-};
-
-class Bar extends Component{
-    render(){
-        var myConfig = {
-            type: "bar",
-        	series : this.props.series
-        };
-        applyAttrs(this.props, myConfig);
-        return (<Core id={this.props.id} height={this.props.height} width={this.props.width} data={myConfig} theme={this.props.theme}/>);
-    }
-};
-
-class Scatter extends Component{
-    render(){
-        var myConfig = {
-            type: "scatter",
-        	series : this.props.series
-        };
-        applyAttrs(this.props, myConfig);
-        return (<Core id={this.props.id} height={this.props.height} width={this.props.width} data={myConfig} theme={this.props.theme}/>);
-    }
-};
-
-class Pie extends Component{
-    render(){
-        var myConfig = {
-            type: "pie",
-        	series : this.props.series
-        };
-        applyAttrs(this.props, myConfig);
-        return (<Core id={this.props.id} height={this.props.height} width={this.props.width} data={myConfig} theme={this.props.theme}/>);
-    }
-};
-
-function applyAttrs(oProps, oConfig){
-    if(oProps.legend && oProps.legend.toLowerCase() == "true"){
-        oConfig.plotarea = oConfig.plotarea || {};
-        oConfig.plotarea.marginRight = "150px";
-        oConfig.legend = {
-            maxItems : "4",
-            overflow : "page"
-        };
-    }
-    if(oProps.title){
-        oConfig.title = {
-            text : oProps.title
-        }
-    }
-}
 export {
-    Core as core,
-    Line as line,
-    Area as area,
-    Bar as bar,
-    Pie as pie,
-    Scatter as scatter
+    ZingChart
 };
