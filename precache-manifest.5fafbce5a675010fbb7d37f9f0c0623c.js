1c8877e762fed6563d254386f8f836d3975b44c4
Nick Ardecky <nardecky@zingsoft.com>
2020-01-27 16:44:50 -0800

    Updates

---
+++ precache-manifest.5fafbce5a675010fbb7d37f9f0c0623c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e27e11c0c40f7a1473b6893ea9d45a",
+    "url": "/zingchart-react/index.html"
+  },
+  {
+    "revision": "197eed7cf1addddc0ac9",
+    "url": "/zingchart-react/static/css/main.8cd5a328.chunk.css"
+  },
+  {
+    "revision": "ae6eb0ede50ba98e6b94",
+    "url": "/zingchart-react/static/js/2.33ef1e28.chunk.js"
+  },
+  {
+    "revision": "8383f2b4838fcff770dc7ca1b7b9e1c6",
+    "url": "/zingchart-react/static/js/2.33ef1e28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "197eed7cf1addddc0ac9",
+    "url": "/zingchart-react/static/js/main.81cc0ee4.chunk.js"
+  },
+  {
+    "revision": "77304ae7ef5dc8e79a29",
+    "url": "/zingchart-react/static/js/runtime-main.4d4b6b9a.js"
+  }
+]);
