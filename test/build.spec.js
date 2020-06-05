45383494d56a8d8f2574cdd5107a890fbc0d0484
Nick Ardecky <nardecky@zingsoft.com>
2020-06-05 00:13:11 -0700

    test: fix test for new build
    No longer need to test modules folder output and no longer need to output modules to the dist folder

--- test/build.spec.js
+++ test/build.spec.js
@@ -15,31 +15,8 @@ describe('Build', function() {
     it(`default export exists`, async function() {
       // exports.ZC = ZC$2;
       let zcReactFilePath = 'dist/zingchart-react.js';
-      expect(zcReactFilePath).to.be.a.file().with.contents.that.match(/exports.default = ZingChart;/);
-    });
-    // verify ZC export exists
-    it(`ZC export exists`, async function() {
-      let zcReactFilePath = 'dist/zingchart-react.js';
-      expect(zcReactFilePath).to.be.a.file().with.contents.that.match(/exports.ZC = ZC\$2;/);
+      expect(zcReactFilePath).to.be.a.file().with.contents.that.match(/module.exports = ZingChart;/);
     });
   });
 
-  describe('dist/modules', function() {
-    // verify the zingchart modules exist
-    it(`zingchart modules folder should exist`, async function() {
-      let zcModulesFilePath = 'dist/modules/';
-      expect(zcModulesFilePath).to.be.a.directory();
-    });
-
-    // verify a couple of the zingchart module files exist
-    it(`zingchart modules files should exist`, async function() {
-      let zcModulesFilePath = 'dist/modules/';
-      let zcModuleFiles = [
-        'zingchart-api-rules.min.js',
-        'zingchart-maps-aus.min.js',
-        'zingchart-sunburst.min.js'
-      ];
-      expect(zcModulesFilePath).to.be.a.directory().and.include.files(zcModuleFiles);
-    });
-  });
 });
