4e4faf3d83f9f610e7f581cb220b1bfebc182568
Bram Goedvriend <bram.goedvriend@gmail.com>
2023-12-04 08:57:36 +0100

    Reconfigure rollup module configuration
    - Prefer ESM of ES
    - Removed sourcemaps
    
    - Updated unit tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,36 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import babel from "rollup-plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.jsx',
+  input: "src/index.jsx",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
+      format: "cjs",
     },
     {
       file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
+      format: "esm",
+    },
   ],
   plugins: [
     external(),
     postcss({
-      modules: true
+      modules: true,
     }),
     url(),
     svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      exclude: "node_modules/**",
     }),
     resolve(),
-    commonjs()
-  ]
-}
+    commonjs(),
+  ],
+};
--- test/build.spec.js
+++ test/build.spec.js
@@ -1,22 +1,22 @@
-const utils = require('./utils.js');
-const chai = require('chai');
-chai.use(require('chai-fs'));
+const utils = require("./utils.js");
+const chai = require("chai");
+chai.use(require("chai-fs"));
 
 // server/controllers/api/card.js
-describe('Build', function() {
-
-  describe('dist/zingchart-react.js', function() {
+describe("Build", function () {
+  describe("dist/zingchart-react.js", function () {
     // verify the ZingChart object exists
-    it(`zingchart-react.js file should exist`, async function() {
-      let zcReactFilePath = 'dist/zingchart-react.js';
+    it(`zingchart-react.esm.js file should exist`, async function () {
+      let zcReactFilePath = "dist/zingchart-react.esm.js";
       expect(zcReactFilePath).to.be.a.path();
     });
     // verify exports
-    it(`default export exists`, async function() {
+    it(`default export exists`, async function () {
       // exports.ZC = ZC$2;
-      let zcReactFilePath = 'dist/zingchart-react.js';
-      expect(zcReactFilePath).to.be.a.file().with.contents.that.match(/module.exports = ZingChart;/);
+      let zcReactFilePath = "dist/zingchart-react.cjs.js";
+      expect(zcReactFilePath)
+        .to.be.a.file()
+        .with.contents.that.match(/module.exports = ZingChart;/);
     });
   });
-
 });
